ba50c5325794dae7a4f73feb522b04f3e18aedd0
Nicklas <nicklas@peqo.io>
2017-03-27 16:19:30 +0200

    Added JSON annotations to User Model

--- app/src/main/java/io/peqo/kbahelper/model/User.java
+++ app/src/main/java/io/peqo/kbahelper/model/User.java
@@ -1,7 +1,10 @@
 package io.peqo.kbahelper.model;
 
-import java.util.List;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import com.fasterxml.jackson.databind.annotation.JsonPOJOBuilder;
 
+@JsonDeserialize(builder = User.Builder.class)
 public class User {
 
     public final Long id;
@@ -9,7 +12,6 @@ public class User {
     public final String lastName;
     public final String username;
     public final String password;
-    public final List<Requisition> requisitions;
 
     private User(Builder builder) {
         this.id = builder.id;
@@ -17,44 +19,43 @@ public class User {
         this.lastName = builder.lastName;
         this.username = builder.username;
         this.password = builder.password;
-        this.requisitions = builder.requisitions;
     }
 
+    @JsonPOJOBuilder(withPrefix = "set")
     public static class Builder {
         private Long id;
         private String firstName;
         private String lastName;
         private String username;
         private String password;
-        private List<Requisition> requisitions;
 
+        @JsonProperty("id")
         public Builder setId(Long id) {
             this.id = id;
             return this;
         }
 
+        @JsonProperty("first_name")
         public Builder setFirstName(String firstName) {
             this.firstName = firstName;
             return this;
         }
 
+        @JsonProperty("last_name")
         public Builder setLastName(String lastName) {
             this.lastName = lastName;
             return this;
         }
 
+        @JsonProperty("username")
         public Builder setUsername(String username) {
             this.username = username;
             return this;
         }
 
+        @JsonProperty("password")
         public Builder setPassword(String password) {
             this.password = password;
-            return this;
-        }
-
-        public Builder setRequisitions(List<Requisition> requisitions) {
-            this.requisitions = requisitions;
             return this;
         }
 
