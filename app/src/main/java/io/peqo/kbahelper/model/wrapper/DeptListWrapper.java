ebc044ac71c16e4c16faaf936492deeed4895ccd
Nicklas <nicklas@peqo.io>
2017-03-27 14:47:56 +0200

    Added JSON notations to Dept List Wrapper

--- app/src/main/java/io/peqo/kbahelper/model/wrapper/DeptListWrapper.java
+++ app/src/main/java/io/peqo/kbahelper/model/wrapper/DeptListWrapper.java
@@ -1,5 +1,6 @@
 package io.peqo.kbahelper.model.wrapper;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
 import com.fasterxml.jackson.databind.annotation.JsonPOJOBuilder;
 
@@ -7,11 +8,13 @@ import com.fasterxml.jackson.databind.annotation.JsonPOJOBuilder;
 public class DeptListWrapper {
 
     public final Long id;
+    public final String name;
     public final int unfinishedReq;
     public final int totalReq;
 
     private DeptListWrapper(Builder builder) {
         this.id = builder.id;
+        this.name = builder.name;
         this.unfinishedReq = builder.unfinishedReq;
         this.totalReq = builder.totalReq;
     }
@@ -20,19 +23,29 @@ public class DeptListWrapper {
     public static class Builder {
 
         private Long id;
+        private String name;
         private int unfinishedReq;
         private int totalReq;
 
+        @JsonProperty("id")
         public Builder setId(Long id) {
             this.id = id;
             return this;
         }
 
+        @JsonProperty("dept_name")
+        public Builder setName(String name) {
+            this.name = name;
+            return this;
+        }
+
+        @JsonProperty("unfinished_req")
         public Builder setUnfinishedReq(int unfinishedReq) {
             this.unfinishedReq = unfinishedReq;
             return this;
         }
 
+        @JsonProperty("finished_req")
         public Builder setTotalReq(int totalReq) {
             this.totalReq = totalReq;
             return this;
