cc6f531d8e3f9577417d4b771a6e6fead096bf03
Nicklas <nicklas@peqo.io>
2017-03-27 14:07:36 +0200

    Added Wrapper class for Dept. Overview

--- app/src/main/java/io/peqo/kbahelper/model/wrapper/DeptListWrapper.java
+++ app/src/main/java/io/peqo/kbahelper/model/wrapper/DeptListWrapper.java
@@ -1,8 +1,45 @@
 package io.peqo.kbahelper.model.wrapper;
 
-/**
- * Created by Nicklas on 27-03-2017.
- */
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import com.fasterxml.jackson.databind.annotation.JsonPOJOBuilder;
 
+@JsonDeserialize(builder = DeptListWrapper.Builder.class)
 public class DeptListWrapper {
+
+    public final Long id;
+    public final int unfinishedReq;
+    public final int totalReq;
+
+    private DeptListWrapper(Builder builder) {
+        this.id = builder.id;
+        this.unfinishedReq = builder.unfinishedReq;
+        this.totalReq = builder.totalReq;
+    }
+
+    @JsonPOJOBuilder(withPrefix = "set")
+    public static class Builder {
+
+        private Long id;
+        private int unfinishedReq;
+        private int totalReq;
+
+        public Builder setId(Long id) {
+            this.id = id;
+            return this;
+        }
+
+        public Builder setUnfinishedReq(int unfinishedReq) {
+            this.unfinishedReq = unfinishedReq;
+            return this;
+        }
+
+        public Builder setTotalReq(int totalReq) {
+            this.totalReq = totalReq;
+            return this;
+        }
+
+        public DeptListWrapper build() {
+            return new DeptListWrapper(this);
+        }
+    }
 }
